33b1b0f98a1710721a84b8f0693413197b42b243
Bjornhona <asaeri3@hotmail.com>
2019-04-14 07:31:53 +0200

    all files added and done

---
+++ src/containers/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+      return response.json()
+    })
+    .then((users) => {
+      this.setState({
+        robots: users
+      })
+    });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({
+      searchfield: event.target.value
+    })
+  }
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ?
+      <h1>Loading...</h1> :
+     (
+        <div className="App">
+          <div>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <Scroll>
+              <CardList robots={filteredRobots}/>
+            </Scroll>
+          </div>
+        </div>
+      );
+  }
+}
+
+export default App;
---
+++ src/lib/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sicere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Bret',
+    email: 'Sicere@april.biz'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Bret',
+    email: 'Sicere@april.biz'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Bret',
+    email: 'Sicere@april.biz'
+  },
+  {
+    id: 5,
+    name: 'Chesley Dietrich',
+    username: 'Bret',
+    email: 'Sicere@april.biz'
+  },
+  {
+    id: 6,
+    name: 'Supercool Me',
+    username: 'Bret',
+    email: 'Sicere@april.biz'
+  }
+]
